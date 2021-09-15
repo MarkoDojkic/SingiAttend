84ea19353e3de2d8064a3a1ab8082657e1ab6ea3
Marko Dojkić <marko.dojkic@gmail.com>
2021-09-15 16:08:42 +0200

    Finished project

---
+++ static/miscellaneous/SingiAttend Server REST/src/main/java/dev/markodojkic/singiattend/server/entity/AttendanceSubobjectInstance.java
@@ -0,0 +1,10 @@
+package dev.markodojkic.singiattend.server.entity;
+
+public interface AttendanceSubobjectInstance {
+    int getSubjectId();
+    String getNameT();
+    String getNameA();
+    String getTitle();
+    String getTitleEnglish();
+    int getIsInactive();
+}
