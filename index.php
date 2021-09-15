c31b212824421a0fcd4aeda4bdf6624027004b27
Marko Dojkić <marko.dojkic@gmail.com>
2021-09-15 16:08:42 +0200

    Finished project

--- index.php
+++ index.php
@@ -146,7 +146,7 @@
         $tBody = "";
         
         $query = sprintf("SELECT subject_id, title, title_english, staff.name_surname as assistant_name_surname, is_inactive FROM `subject`
-        INNER JOIN staff ON `subject`.assistant_id = staff.staff_id
+        LEFT JOIN staff ON `subject`.assistant_id = staff.staff_id
         WHERE professor_id = '%s';", mysqli_real_escape_string($conn,$_SESSION['loggedInId']));
 
         $data = $conn->query($query);
