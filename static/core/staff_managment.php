548960240ccf1c0a4fcb0169e72eff5daa8a5098
Marko Dojkić <marko.dojkic@gmail.com>
2021-09-20 20:11:32 +0200

    Logo, index.php and language fixes. Readme update.

--- static/core/staff_managment.php
+++ static/core/staff_managment.php
@@ -45,7 +45,7 @@
     function editStaffMember($id,$xml,$conn){
 
         $nameSurname_pattern = "/^([\x{0410}-\x{0418}\x{0402}\x{0408}\x{041A}-\x{041F}\x{0409}\x{040A}\x{0420}-\x{0428}\x{040B}\x{040F}A-Z\x{0110}\x{017D}\x{0106}\x{010C}\x{0160}]{1}[\x{0430}-\x{0438}\x{0452}\x{043A}-\x{043F}\x{045A}\x{0459}\x{0440}-\x{0448}\x{0458}\x{045B}\x{045F}a-z\x{0111}\x{017E}\x{0107}\x{010D}\x{0161}]+(\s|\-)?)+$/u";
-        $password_pattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/";
+        $password_pattern = "/^(?=.*[a-z])(?=.*\d)[a-z\d]{8,}$/";
 
         if($_POST["newNS_$id"] !== null && preg_match_all($nameSurname_pattern, $_POST["newNS_$id"])){
             $url = "http://127.0.0.1:62812/api/update/staff/" . $id;
@@ -90,17 +90,6 @@
 
             $response = file_get_contents($url, false, $context);
             if(json_decode($response)->{"error"} != null) showErrorAlert($xml->errors->wrong_pass[0]);
-            $date = date('d.m.Y H:i:s', time()); //H - 24, h - 12
-            $newPass = $_POST["newPASS_$id"];
-            $editData = "
-                ------PASSWORD_CHANGED_STAFF------
-                        ID: {$id}
-              NEW PASSWORD: $newPass
-                 CHANGE AT: {$date}
-                ------PASSWORD_CHANGED_STAFF------                  
-            ";
-
-            file_put_contents(DIR_ROOT . DIR_MISCELLANEOUS . "/editedPasswords.rtf", $editData, FILE_APPEND | LOCK_EX);
         }
 
         reloadPage();   
