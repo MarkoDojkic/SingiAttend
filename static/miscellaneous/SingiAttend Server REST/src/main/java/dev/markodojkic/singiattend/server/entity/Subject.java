52086ece90073ecf19d7981b16e09c9b086d25a7
Marko Dojkić <marko.dojkic@gmail.com>
2022-01-30 15:25:46 +0100

    Fixed date time issue when showing course data

--- static/miscellaneous/SingiAttend Server REST/src/main/java/dev/markodojkic/singiattend/server/entity/Subject.java
+++ static/miscellaneous/SingiAttend Server REST/src/main/java/dev/markodojkic/singiattend/server/entity/Subject.java
@@ -1,30 +1,38 @@
 package dev.markodojkic.singiattend.server.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.Type;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import javax.persistence.*;
-import java.sql.Date;
+import java.util.ArrayList;
 
 @Data
 @Document(collection = "Subjects")
 public class Subject {
     @Id
-    private int id;
+    private String id;
     @Field("title")
     private String title;
     @Field("title_english")
     private String title_english;
-    @Field("professor_id")
+    @Field(targetType = FieldType.OBJECT_ID, value = "professor_id")
     private String professorId;
-    @Field("assistant_id")
+    @Field(targetType = FieldType.OBJECT_ID, value = "assistant_id")
     private String assistantId;
     @Field("last_lecture_at")
     private String lastLectureAt;
     @Field("last_exercise_at")
     private String lastExerciseAt;
-    @Field("is_inactive")
-    private boolean isInactive;
+    @Type(type = "string-array")
+    @Field("enroled_students")
+    private ArrayList<String> enroled_students;
+    @Field("isInactive")
+    private String isInactive;
+    @Field("professor")
+    private ArrayList<Staff> professor;
+    @Field("assistant")
+    private ArrayList<Staff> assistant;
 }
--- static/miscellaneous/SingiAttend Server REST/src/main/java/dev/markodojkic/singiattend/server/repository/ISubjectRepository.java
+++ static/miscellaneous/SingiAttend Server REST/src/main/java/dev/markodojkic/singiattend/server/repository/ISubjectRepository.java
@@ -1,20 +1,33 @@
 package dev.markodojkic.singiattend.server.repository;
 
 import dev.markodojkic.singiattend.server.entity.AttendanceSubobjectInstance;
-import dev.markodojkic.singiattend.server.entity.CourseDataInstance;
+import dev.markodojkic.singiattend.server.entity.CourseDataLecture;
 import dev.markodojkic.singiattend.server.entity.Subject;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
-
-import java.util.List;
-
 @EnableMongoRepositories
 public interface ISubjectRepository extends MongoRepository<Subject, String> {
-    @Aggregation("{ $match : { 'enroled_students' : { $eq: ?0 } } } }, {$lookup: {from:'Staff',localField:'professor_id',foreignField:'_id',as:'professor'}},{$lookup: {from:'Staff',localField:'assistant_id',foreignField:'_id',as:'assistant'}}])})")
-    List<CourseDataInstance> getCourseData(String index);
-    @Aggregation("{ $lookup: {from:'Staff',localField:'professor_id',foreignField:'_id',as:'professor'}},{$lookup: {from:'Staff',localField:'assistant_id',foreignField:'_id',as:'assistant'}},{$match: {enroled_students: {$eq: ?0}} }")
-    List<AttendanceSubobjectInstance> getSubobjectdata(String index);
+    @Aggregation(pipeline={"{$lookup: {from:'Staff',localField:'professor_id',foreignField:'_id',as:'professor'}}","{$lookup: {from:'Staff',localField:'assistant_id',foreignField:'_id',as:'assistant'}}","{$match: {enroled_students: {$eq: ?0}} }"})
+    AggregationResults<AttendanceSubobjectInstance> getSubobjectdata(String id);
+    @Aggregation(pipeline={"{  '$addFields': {  'date': \n" +
+            "{ '$toLong':{  '$dateFromString': { 'dateString': {  $cond: { if: { $eq: [ '$last_lecture_at',''] }, then: '1-1-1970+0000', else: '$last_lecture_at' } } } } } } } \n",
+            "{ $match : { 'enroled_students' : { $eq: ?0 } } } \n",
+            "{ $match : { 'date' : { $gte: ?1 } } } \n",
+            "{$lookup: {from:'Staff',localField:'professor_id',foreignField:'_id',as:'professor'}} \n",
+            "{$lookup: {from:'Staff',localField:'assistant_id',foreignField:'_id',as:'assistant'}} \n",
+            "{$project: {'professor_id':0,'assistant_id':0,'date':0,'enroled_students':0, '_class':0}}"})
+    AggregationResults<CourseDataLecture> getCourseDataLectures(String studentId, long fromDate);
+    @Aggregation(pipeline={"{  '$addFields': {  'date': \n" +
+            "{ '$toLong':{  '$dateFromString': { 'dateString': {  $cond: { if: { $eq: [ '$last_exercise_at',''] }, then: '1-1-1970+0000', else: '$last_exercise_at' } } } } } } } \n",
+            "{ $match : { 'enroled_students' : { $eq: ?0 } } } \n",
+            "{ $match : { 'date' : { $gte: ?1 } } } \n",
+            "{$lookup: {from:'Staff',localField:'professor_id',foreignField:'_id',as:'professor'}} \n",
+            "{$lookup: {from:'Staff',localField:'assistant_id',foreignField:'_id',as:'assistant'}} \n",
+            "{$project: {'professor_id':0,'assistant_id':0,'date':0,'enroled_students':0,'_class':0}}"})
+    AggregationResults<CourseDataLecture> getCourseDataExercises(String studentId, long fromDate);
+    @Aggregation(pipeline={"{$lookup: {from:'Staff',localField:'professor_id',foreignField:'_id',as:'professor'}}","{$lookup: {from:'Staff',localField:'assistant_id',foreignField:'_id',as:'assistant'}}"})
+    AggregationResults<Subject> findAllAggregation();
 }
