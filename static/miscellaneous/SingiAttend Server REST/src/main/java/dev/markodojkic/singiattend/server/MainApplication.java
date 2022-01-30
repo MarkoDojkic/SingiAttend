0308978209b4ee937ad6c70ac4190fdf21b78822
Marko Dojkić <marko.dojkic@gmail.com>
2022-01-30 15:25:46 +0100

    Fixed date time issue when showing course data

--- static/miscellaneous/SingiAttend Server REST/src/main/java/dev/markodojkic/singiattend/server/MainApplication.java
+++ static/miscellaneous/SingiAttend Server REST/src/main/java/dev/markodojkic/singiattend/server/MainApplication.java
@@ -2,8 +2,10 @@ package dev.markodojkic.singiattend.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
