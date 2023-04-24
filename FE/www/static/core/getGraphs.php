0eb6901bd6b89f1aa948fac41f7aa8893c2395ae
Marko Dojkić <marko.dojkic@gmail.com>
2023-04-24 09:59:44 +0200

    Dockerized teacher app and BE

---
+++ FE/www/static/core/getGraphs.php
@@ -0,0 +1,170 @@
+<?php
+
+    session_start();
+    require "../../constants.php";
+
+    $xml = @simplexml_load_file(DIR_ROOT . DIR_LANGUAGES . "/{$_SESSION["language"]}.xml")  or die(file_get_contents(DIR_ROOT . "/error404.html"));
+    $attended = "";
+    $notAttended = "";
+    $attendancesData = "";
+    $datesData = "[";
+
+    $url = "http://" . SERVER_URL . SERVER_PORT . "/api/totalStudents/" . $_POST["subjectSelection"];
+                
+    $context = stream_context_create(array(
+        "http" => array(
+            "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+            "protocol_version" => 1.1,
+            'method' => 'GET'
+    )));
+
+    $totalStudents = json_decode(@file_get_contents($url, false, $context));
+
+    $attendedTotalLectures = 0;
+    $totalStudentsAllLectures = 0;
+
+    $suffix = strcmp($_SESSION["loggedInAs"], "professor") === 0 ? "Lectures" : "Exercises";
+
+    
+    $url = "http://" . SERVER_URL . SERVER_PORT . "/api/getAll" . $suffix . "/" . $_POST["subjectSelection"];
+
+    $context = stream_context_create(array(
+        "http" => array(
+            "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+            "protocol_version" => 1.1,
+            'method' => 'GET'
+    )));
+
+    $lectures = json_decode(@file_get_contents($url, false, $context), true);
+
+    if(!is_array($lectures) || sizeof($lectures) == 0) die("<i style='color:red;font-size:28px;'>" . $xml->errors->graphNotGenerated[0] . "</i>");
+    
+    foreach($lectures as $lecture){
+        $attended .= "\"" . sizeof($lecture["attended_students"]) . "\",";
+        $notAttended .= "\"" . ($totalStudents - sizeof($lecture["attended_students"])) . "\",";
+        $datesData .= "\"" . explode("T", $lecture['started_at'])[0] .  "\",";
+        $attendedTotalLectures += sizeof($lecture["attended_students"]);
+        $totalStudentsAllLectures += $totalStudents;
+    }
+
+    $attended = substr($attended,0,strlen($attended)-1); //remove last ,
+    $notAttended = substr($notAttended,0,strlen($notAttended)-1);
+
+    $attendancesData = "[{
+        label: '{$xml->professorPage->attended[0]}',
+        backgroundColor: '#33FF3F',
+        data: [$attended]
+    },{
+        label: '{$xml->professorPage->notAttended[0]}',
+        backgroundColor: '#FF3333',
+        data: [$notAttended]
+    }]";
+
+    $datesData .= "]";
+    
+    if($_POST["graphValue"] === "barGraph"){
+        echo "
+                <script src='https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js'></script>
+                <canvas id='myChart' style='position: relative; height:80vh; width:82vw;'></canvas>
+                <script>
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: {$datesData},
+                        datasets: {$attendancesData}
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        title: {
+                            display: true,
+                            text: '{$xml->{$_SESSION["loggedInAs"] . "Page"}->barChartTitle[0]}'
+                        }
+                    }
+                });
+                </script>
+            ";
+    }
+    else if($_POST["graphValue"] === "pieGraph"){
+        $attendedTotalExercieses = 0;
+        $totalStudentsAllExercieses = 0;
+
+        $url = "http://" . SERVER_URL . SERVER_PORT . "/api/getAllExercises/" . $_POST["subjectSelection"];
+                
+        $context = stream_context_create(array(
+            "http" => array(
+                "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+                "protocol_version" => 1.1,
+                'method' => 'GET'
+        )));
+
+        $exercises = json_decode(file_get_contents($url, false, $context), true);
+
+        if(!is_array($exercises) || $exercises.length == 0) die("<i style='color:red;font-size:28px;'>" . $xml->errors->graphNotGenerated[0] . "</i>");
+
+        foreach($exercises as $exercise){
+            $attended .= "\"" . sizeof($exercise["attended_students"]) . "\",";
+            $notAttended .= "\"" . ($totalStudents - sizeof($exercise["attended_students"])) . "\",";
+            $datesData .= "\"" . explode("T", $exercise['started_at'])[0] .  "\",";
+            $attendedTotalExercieses += sizeof($exercise["attended_students"]);
+            $totalStudentsAllExercieses += $totalStudents;
+        }
+
+        $percentageAL = round($attendedTotalLectures/$totalStudentsAllLectures*100);
+        $percentageNAL = 100-$percentageAL;
+        $percentageAE = round($attendedTotalExercieses/$totalStudentsAllExercieses*100);
+        $percentageNAE = 100-$percentageAE;
+
+        $label1 = $xml->professorPage->attended[0] . " " . strtolower($xml->professorPage->lecturesBtn[0]);
+        $label2 = $xml->professorPage->notAttended[0] . " " . strtolower($xml->professorPage->lecturesBtn[0]);
+        $label3 = $xml->professorPage->attended[0] . " " . strtolower($xml->assistantPage->exercisesBtn[0]);
+        $label4 = $xml->professorPage->notAttended[0] . " " . strtolower($xml->assistantPage->exercisesBtn[0]);
+
+        echo "
+                <script src='https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js'></script>
+                <canvas id='myChart' style='position: relative; height:81vh; width:164vw;'></canvas>
+                <canvas id='myChart2' style='position: relative; height:80vh; width:164vw;'></canvas>
+                <script>
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'pie',
+                    data: {
+                        labels: ['$label1','$label2'],
+                        datasets: [
+                            {backgroundColor: ['#33FF3F','#FF3333'], data : [$percentageAL,$percentageNAL] }
+                        ]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: '{$xml->professorPage->pieChartTitle[0]}'
+                        }
+                    }
+                });
+                var ctx = document.getElementById('myChart2').getContext('2d');
+                var myChart2 = new Chart(ctx, {
+                    type: 'pie',
+                    data: {
+                        labels: ['$label3','$label4'],
+                        datasets: [
+                            {backgroundColor: ['#99ffa0','#ff8080'], data : [$percentageAE,$percentageNAE] }
+                        ]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: '{$xml->assistantPage->pieChartTitle[0]}'
+                        }
+                    }
+                });
+                </script>
+            ";
+    }
+
+?>
---
+++ FE/www/static/core/teaching_exercises.php
@@ -0,0 +1,239 @@
+<?php
+    session_start();
+    require "../../constants.php";
+
+    $xml = @simplexml_load_file(DIR_ROOT . DIR_LANGUAGES . "/{$_SESSION["language"]}.xml")  or die(file_get_contents(DIR_ROOT . "/error404.html"));
+
+    foreach(array_keys($_POST) as $key){
+        switch(explode('_',$key)[0]){
+            case 'details': showDetails(explode('_',$key)[1],$xml); break 2;
+            case 'viewStudents': viewAttendingStudents(explode('_',$key)[1],$xml); break 2;
+            case 'exercises': viewExercises(explode('_',$key)[1],$xml); break 2;
+            case 'startNE': startNewExercise(explode('_',$key)[1],$xml); break 2;
+            case 'cancel': exit;
+            default: continue 2;
+        }
+    }
+
+    function showDetails($id,$xml){
+        $url = "http://" . SERVER_URL . SERVER_PORT . "/api/getSubject/" . $id;
+                
+        $context = stream_context_create(array(
+            "http" => array(
+                "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+                "protocol_version" => 1.1,
+                'method' => 'GET'
+        )));
+
+        $data = json_decode(file_get_contents($url, false, $context), true);
+
+        $action = DIR_ROOT_ONLY . DIR_CORE . "/teaching_exercises.php";
+
+        $professor = $data["professor"][0]["name_surname"];
+
+        echo "
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js'></script>
+        <script>
+            var rows = window.top.document.querySelectorAll('tbody tr');
+            var i;
+            for (i = 0; i < rows.length; i++) {
+                if(rows[i].id != 'tr_{$id}') rows[i].style.backgroundColor = '#00000000';
+                else rows[i].style.backgroundColor = 'lightblue';
+            }
+        </script>
+        <form action='{$action}' method='post' style='text-align: center;margin:0px auto; background-color: #cdeaff; height: 100%;'>
+            <div class='form-group'>
+                <label for='subject_name'>{$xml->professorPage->subject_name[0]}:</label><br><br>
+                <input type='text' class='form-control' name='subject_name' id='subject_name' autocomplete='subject_name' placeholder='{$data["title"]}' readonly>/
+                <input type='text' class='form-control' name='subject_nameEng' id='subject_nameEng' autocomplete='subject_nameEng' placeholder='{$data["title_english"]}' readonly>
+            </div>
+            <div class='form-group'>
+                <label for='professor'>{$xml->registrationPage->professor[0]}:</label>
+                <input type='text' class='form-control' name='professor_name' id='professor_name' placeholder='{$professor}' readonly>
+            </div>
+            <br>		
+            <div class='form-group'>
+                <button type='submit' class='btn btn-info' id='viewStudents_$id' name='viewStudents_$id'>{$xml->assistantPage->viewStudentsBtn[0]}</button>
+                <button type='submit' class='btn btn-danger' id='cancel' name='cancel'>{$xml->professorPage->cancelBtn[0]}</button>
+            </div>
+        </form>";
+    }  
+
+    function viewAttendingStudents($id,$xml){
+        $alert = "{$xml->assistantPage->viewStudentsText[0]}";
+        $localization = $_SESSION["language"] === "english" ? "title_english" : "title";
+        
+        $url = "http://" . SERVER_URL . SERVER_PORT . "/api/allStudentBySubjectId/" . $id;
+                
+        $context = stream_context_create(array(
+            "http" => array(
+                "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+                "protocol_version" => 1.1,
+                'method' => 'GET'
+        )));    
+
+        $data = json_decode(file_get_contents($url, false, $context), true);
+
+        foreach($data as $attendingStudent){
+            $lang = $attendingStudent["study"]["taught_in"] === "engleski" ? $xml->professorPage->englishStudent[0] : $xml->professorPage->serbianStudent[0];
+            $study = $_SESSION["language"] === "english" ? 
+            $attendingStudent["study"][0]["faculty_title_english"] . ", " . $attendingStudent["study"][0]["title_english"] . ", " . $lang:
+            $attendingStudent["study"][0]["faculty_title"] . ", " . $attendingStudent["study"][0]["title"] . ", " . $lang;
+
+            $alert .= "\\n - {$attendingStudent["name_surname"]} ({$attendingStudent["index"]}) - {$attendingStudent["faculty"]} ({$study})";
+        }
+
+        if(!strpos($alert,'-')) $alert = $xml->assistantPage->schoolYearOver[0];
+        //returns 1 (true) if - does not exist in the string (i.e. in the case if there are no students or if the subject is inactive - subject.is_inactive = '1'
+        
+        echo "<script>alert('{$alert}');</script>";
+        showDetails($id,$xml);
+    }
+
+    function viewExercises($id,$xml){
+
+        $url = "http://" . SERVER_URL . SERVER_PORT . "/api/subjectIsInactiveById/" . $id;
+                
+        $context = stream_context_create(array(
+            "http" => array(
+                "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+                "protocol_version" => 1.1,
+                'method' => 'GET'
+        )));
+
+        $data = file_get_contents($url, false, $context);
+
+        if($data === 'true') 
+            die ("
+            <script>
+            alert('{$xml->assistantPage->schoolYearOver[0]}');
+            
+            setTimeout(function(){
+                window.top.location.reload();
+            }, 1);</script>");
+
+        $exercises = "<option value=''>-</option>";
+
+        $url = "http://" . SERVER_URL . SERVER_PORT . "/api/getAllExercises/" . $id;
+                
+        $context = stream_context_create(array(
+            "http" => array(
+                "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+                "protocol_version" => 1.1,
+                'method' => 'GET'
+        )));
+
+        $data = json_decode(file_get_contents($url, false, $context), true);
+        
+        foreach($data as $exercise){
+            $exercises .= "<option value='{$exercise['id']}'>ID: {$exercise['id']}</option>";
+        }
+
+        $dateLocal = $_SESSION["language"] === "english" ? date("Y-m-d") : date("d.m.Y");
+
+        $label1 = $dateLocal . ' ' . $xml->assistantPage->startTime[0];
+        $label2 = $dateLocal . ' ' . $xml->assistantPage->endTime[0];
+
+        $action = DIR_ROOT_ONLY . DIR_CORE . "/teaching_exercises.php";
+        $ajax_url = "getLEInfo.php";
+
+        echo "
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js'></script>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+        <script>
+            var rows = window.top.document.querySelectorAll('tbody tr');
+            var i;
+            for (i = 0; i < rows.length; i++) {
+                if(rows[i].id != 'tr_{$id}') rows[i].style.backgroundColor = '#00000000';
+                else rows[i].style.backgroundColor = 'lightblue';
+            }
+
+            function updateExerciseInfo(selections){
+                if(selections[selections.selectedIndex].value === '') {
+                    document.querySelector('#exerciseInfo').innerHTML = '';
+                    return;
+                }
+                $.ajax({
+                    type: 'POST',
+                    url: '{$ajax_url}',
+                    data: {'exercise_id':selections[selections.selectedIndex].value}, 
+                    success: function(result){
+                        document.querySelector('#exerciseInfo').innerHTML = result;
+                    }
+                });
+            }
+        </script>
+        
+        <form target='phpIframe' action='{$action}' method='post' style='text-align: center;margin:0px auto; background-color: #cdeaff; height: 100%;'>
+            <br>
+            <div class='form-group'>
+                <label for='exerciseSelection'>{$xml->assistantPage->exerciseSelection[0]}:</label>
+                <select class='form-control' name='exerciseSelection' id='exerciseSelection' onchange='updateExerciseInfo(this)'>
+                    {$exercises}
+                </select>
+            </div>
+            <div id='exerciseInfo'></div>
+            <div class='form-group'>
+                <label for='start_time'>{$label1}</label>
+                <input type='time' id='start_time' name='start_time'>
+            </div>
+            <div class='form-group'>
+                <label for='end_time'>{$label2}</label>
+                <input type='time' id='end_time' name='end_time'>
+            </div>			
+            <div class='form-group'>
+                <button type='submit' class='btn btn-success' id='startNE_$id' name='startNE_$id'>{$xml->assistantPage->exerciseStartBtn[0]}</button>
+                <button type='submit' class='btn btn-danger' id='cancel' name='cancel'>{$xml->professorPage->cancelBtn[0]}</button>
+            </div>
+        </form>
+        ";
+    }
+
+    function startNewExercise($id,$xml){
+
+        $url = "http://" . SERVER_URL . SERVER_PORT . "/api/getLastExercise/" . $id;
+                
+        $context = stream_context_create(array(
+            "http" => array(
+                "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+                "protocol_version" => 1.1,
+                'method' => 'GET'
+        )));
+
+        $lastExerciseData = json_decode(file_get_contents($url, false, $context), true);
+
+        $exerciseLength = date_diff(new DateTime($_POST['start_time']),new DateTime($_POST['end_time']));
+        
+        $lengthFromLastExercise = date_diff(date_create('2015-01-26 13:15:00'),date_create('2015-01-26 13:15:00'));
+        //defaultly set so it'll have invert parametar 0 to pass if test below
+
+        if($lastExerciseData != null)
+            $lengthFromLastExercise = date_diff(new DateTime($lastLectureData["ended_at"]),new DateTime($_POST['start_time'])); 
+
+        if($exerciseLength->i < 45 && $exerciseLength->h == 0 // incorrect: 12:00->12:44;12:00->11:59;12:00->18:01
+            || $exerciseLength->invert === 1 || $exerciseLength->h > 6 || $exerciseLength->h == 6 && $exerciseLength->i != 0 || $lengthFromLastExercise->invert === 1) {
+            echo "<script>alert('{$xml->errors->LELengthInvalid[0]}');</script>";
+            viewExercises($id,$xml,$conn);
+            exit;
+        }
+
+        $url = "http://" . SERVER_URL . SERVER_PORT . "/api/insert/exercise/" . $id . "/" . $_POST['start_time'] . "/" . $_POST['end_time'];
+            
+        $context = stream_context_create(array(
+            "http" => array(
+                "header" => "Authorization: Basic " . base64_encode(SERVER_USERNAME . ":" . SERVER_PASSWORD) . "\r\nContent-Type: application/json",
+                "protocol_version" => 1.1,
+                'method' => 'PUT'
+        )));
+
+        file_get_contents($url, false, $context);
+
+        echo("<script>alert('{$xml->assistantPage->startNewExerciseSuccessfull[0]}');</script>");
+
+        viewExercises($id,$xml,$conn);
+    }
+?>
